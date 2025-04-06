baea84da82ebc2ed655297105ee72389bd8aa1b1
peilin16 <h234517683@gmail.com>
2025-04-06 15:10:14 -0700

    experiment1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,43 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+const fillers = {
+  role: ["Game Designer", "Level Architect", "Systems Programmer", "UI Wizard", "Narrative Designer", "Gameplay Engineer", "Tech Artist", "CSGD Student", "Huang"],
+  challenge: ["core loop", "combat system", "city layout", "UI flow", "dialogue tree", "economy balance", "pathfinding AI", "multiplayer sync", "shader code"],
+  problem: ["broken", "unbalanced", "unoptimized", "unintuitive", "buggy", "unfun", "too easy", "too hard", "visually dull"],
+  tool: ["Unity", "Unreal Engine", "Godot", "C++", "JavaScript", "Blueprints", "Shadergraph", "Aseprite", "Blender"],
+  fix: ["refactor", "redesign", "optimize", "debug", "rewrite", "balance", "polish", "test", "document"],
+  reward: ["Steam release", "5-star review", "feature in GameDev Magazine", "job offer", "award nomination", "Twitter fame", "Discord hype", "itch.io feature"],
+  urgency: ["before the deadline", "for the playtest tomorrow", "before the publisher sees it", "for the portfolio review", "before the build submission"],
+  reaction: ["Panic!", "Emergency!", "Help needed!", "URGENT!", "Mayday!", "SOS!", "911!"],
+};
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const template = `$reaction $role Attention!
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+The game's $challenge is completely $problem! 
+We need you to $fix it using $tool $urgency!
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+If you can solve this, we might get our $reward!
+`;
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
+let story = template;
+function generate() {
+  story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
-
-// let's get this party started - uncomment me
-//main();
+$("#box").text(story);
+$("#clicker").click(generate);
